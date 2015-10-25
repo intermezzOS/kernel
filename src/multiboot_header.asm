f1601e66dfbf55c7d8fe3602bf716d5720b581b8
Steve Klabnik <steve@steveklabnik.com>
2015-10-24 21:52:16 -0400

    babby's first multiboot header

---
+++ src/multiboot_header.asm
@@ -0,0 +1,12 @@
+section .multiboot_header
+header_start:
+    dd 0xe85250d6                ; magic number (multiboot 2)
+    dd 0                         ; architecture 0 (protected mode i386)
+    dd header_end - header_start ; header length
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
+header_end:
+
