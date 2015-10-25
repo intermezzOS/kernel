3f4d2c8f6f5a3560c175fefde4fc3e15feddabee
Steve Klabnik <steve@steveklabnik.com>
2015-10-25 00:41:35 -0400

    lol :100:

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -4,6 +4,8 @@ section .text
 bits 32
 start:
     ; print `OK` to screen
-    mov dword [0xb8000], 0x2f4b2f4f
+    ; mov dword [0xb8000], 0x2f4b2f4f
+    mov dword [0xb8000], 0x914F914c
+    mov dword [0xb8004], 0x0000914c
     hlt
 
