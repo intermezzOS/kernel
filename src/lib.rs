3d7aa4beb4b6986b10974377c8ff0f7535b74ea3
Amit Aryeh Levy <amit@amitlevy.com>
2015-11-15 17:27:33 -0500

    Put VGA text console size into const

--- src/lib.rs
+++ src/lib.rs
@@ -18,12 +18,14 @@ pub extern fn kmain() {
     print_center_string(hello, color, vga);
 }
 
+const CONSOLE_SIZE : isize = 4000;
+
 /// Prints a string in the center of the screen.
 ///
 /// Only works for short strings, less than a full line.
 fn print_center_string(s: &str, color: u8, location: *mut u8) {
     // we offset by the length because we need two bytes per letter
-    let mut offset = 2000;
+    let mut offset = CONSOLE_SIZE / 2;
     offset -= s.len() as isize;
 
     let location = unsafe { location.offset(offset) };
