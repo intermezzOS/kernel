9b961d3be458eb44f8d2b740768938d4b707993d
Steve Klabnik <steve@steveklabnik.com>
2015-11-15 17:19:06 -0500

    Refactor printing to a function

--- src/lib.rs
+++ src/lib.rs
@@ -9,20 +9,21 @@ extern crate rlibc;
 
 #[no_mangle]
 pub extern fn kmain() {
-
     let vga = 0xb8000 as *mut u8;
-
-    let color: u8 = 0x4f;
-
+    let color = 0x4f;
     let hello = "Hello from Rust world!";
 
-    for (i, c) in hello.bytes().enumerate() {
-        unsafe {
-            let vga = vga.offset((i * 2) as isize);
-            *vga = c;
+    print_string(hello, color, vga);
+}
 
-            let vga = vga.offset(1 as isize);
-            *vga = color;
+fn print_string(s: &str, color: u8, location: *mut u8) {
+    for (i, c) in s.bytes().enumerate() {
+        unsafe {
+            let location = location.offset((i * 2) as isize);
+            *location = c;
+
+            let location = location.offset(1 as isize);
+            *location = color;
         }
     }
 
