856ec4eb275091332743bae129e49b42d87e4418
Steve Klabnik <steve@steveklabnik.com>
2015-12-20 15:40:51 -0500

    deploy to gh pages on travis push

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd docs/book
+
+git init
+git config user.name "Steve Klabnik"
+git config user.email "steve@steveklabnik.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/steveklabnik/nucleus.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
