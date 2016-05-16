cebc08781980375196754eac43c8557103d20506
Steve Klabnik <steve@steveklabnik.com>
2016-05-15 20:02:37 -0400

    Simplify boot.asm a bit
    So, here's the thing: the code we had before is a bit nicer in that it
    uses functions, but it's also a bit more complex in that we had to set
    up a stack, etc. So I'm going to make the code "worse" in one sense,
    but eliminating a concept or two. It's fine.

--- src/src/arch/x86_64/boot.asm
+++ src/src/arch/x86_64/boot.asm
@@ -4,11 +4,52 @@ extern long_mode_start
 section .boot
 bits 32
 start:
-    mov esp, stack_top
 
-    ; setup paging
-    call set_up_page_tables
-    call enable_paging
+    ; Point the first entry of the level 4 page table to the first entry in the
+    ; p3 table
+    mov eax, p3_table
+    or eax, 0b11 ; 
+    mov dword [p4_table + 0], eax
+
+    ; Point the first entry of the level 3 page table to the first entry in the
+    ; p2 table
+    mov eax, p2_table
+    or eax, 0b11
+    mov dword [p3_table + 0], eax
+
+    ; point each page table level two entry to a page
+    mov ecx, 0         ; counter variable
+.map_p2_table:
+    mov eax, 0x200000  ; 2MiB
+    mul ecx
+    or eax, 0b10000011
+    mov [p2_table + ecx * 8], eax
+
+    inc ecx
+    cmp ecx, 512
+    jne .map_p2_table
+
+    ; move page table address to cr3
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    or eax, 1 << 16
+    mov cr0, eax
+
 
     lgdt [gdt64.pointer]
 
@@ -23,59 +64,6 @@ start:
 
     ; shouldn't ever happen
     hlt
-
-set_up_page_tables:
-    ; P4, P3 and P2
-    ; P4 --> point first entry to (first entry in) P3
-    mov eax, p3_table
-    or eax, 0b11
-    mov dword [p4_table + 0], eax
-
-    ; P3 --> point first entry to (first entry in) P2
-    mov eax, p2_table
-    or eax, 0b11
-    mov dword [p3_table + 0], eax
-
-    ; P2 --> point each entry to a 2MiB page
-    mov ecx, 0         ; counter variable
-.map_p2_table:
-    ; map ecx-th P2 entry to a huge page that starts at address 2MiB*ecx
-    mov eax, 0x200000  ; 2MiB
-    mul ecx            ; start address of ecx-th page
-    or eax, 0b10000011 ; present + writable + huge
-    mov [p2_table + ecx * 8], eax ; map ecx-th entry
-
-    inc ecx            ; increase counter
-    cmp ecx, 512       ; if counter == 512, the whole P2 table is mapped
-    jne .map_p2_table  ; else map the next entry
-
-    ret
-
-
-enable_paging:
-    ; load P4 to cr3 register (cpu uses this to access the P4 table)
-    mov eax, p4_table
-    mov cr3, eax
-
-    ; enable PAE-flag in cr4 (Physical Address Extension)
-    mov eax, cr4
-    or eax, 1 << 5
-    mov cr4, eax
-
-    ; set the long mode bit in the EFER MSR (model specific register)
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1 << 8
-    wrmsr
-
-    ; enable paging in the cr0 register
-    mov eax, cr0
-    or eax, 1 << 31
-    or eax, 1 << 16
-    mov cr0, eax
-
-    ret
-
 
 section .bss
 align 4096
