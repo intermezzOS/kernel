f532ab9a2a40a65043e739eff713454540f432ac
Steve Klabnik <steve@steveklabnik.com>
2016-06-08 20:19:27 -0400

    Move more asm into Rust
    This commit does two things: first, it removes the setup_SSE, as we are
    not actually using SSE instructions. This was introduced when following
    Phil's tutorial long ago, but now we have a libcore that doesn't use SSE
    at all, so this should be fine.
    
    Second, we move the PIC stuff into Rust. It's just some outb/inb calls,
    no need to keep it in assembly.

--- src/src/lib.rs
+++ src/src/lib.rs
@@ -10,11 +10,14 @@ extern crate vga;
 
 extern crate interrupts;
 extern crate keyboard;
+extern crate pic;
 
 pub mod support; // For Rust lang items
 
 #[no_mangle]
 pub extern "C" fn kmain() -> ! {
+    pic::remap();
+
     vga::clear_console();
     vga::initialize_cursor();
 
