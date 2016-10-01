a166de0035be9e1a467622067ec49d0ce280e667
Wilson Giese <giese.wilson@gmail.com>
2016-10-01 15:29:32 -0700

    Fix oob

--- keyboard/src/lib.rs
+++ keyboard/src/lib.rs
@@ -3,7 +3,7 @@
 static CODES: [u8; 59] = *b"??1234567890-=??qwertyuiop[]\n?asdfghjkl;'`?\\zxcvbnm,./?*? ?";
 
 pub fn from_scancode(code: usize) -> Option<char> {
-    if code <= 59 { 
+    if code < 59 { 
         Some(CODES[code] as char)
     } else {
         None
