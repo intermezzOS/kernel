f2200c9600499a8210def165beba6ea7b321064a
Alexander Kuleshov <kuleshovmail@gmail.com>
2016-10-02 23:43:40 +0600

    add constants instead of magic numbers

--- pic/src/lib.rs
+++ pic/src/lib.rs
@@ -4,33 +4,47 @@
 extern crate x86;
 use x86::shared::io::{inb, outb};
 
+/* PIC input/output ports */
+const PIC1_CMD_IO_PORT: u16 = 0x0020;
+const PIC2_CMD_IO_PORT: u16 = 0x00A0;
+const PIC1_DATA_IO_PORT: u16 = 0x0021;
+const PIC2_DATA_IO_PORT: u16 = 0x00A1;
+
+/* PIC commands */
+const ICW1: u8 = 0x11;
+const ICW4: u8 = 0x1;
+
+/* new inetrrupt vector offsets for remmaped PICs */
+const PIC1_VECTOR_OFFSET: u8 = 0x20;
+const PIC2_VECTOR_OFFSET: u8 = 0x28;
+
 pub fn remap() {
     unsafe {
-        let pic1_mask = inb(0x21);
-        let pic2_mask = inb(0xA1);
+        let pic1_mask = inb(PIC1_DATA_IO_PORT);
+        let pic2_mask = inb(PIC2_DATA_IO_PORT);
 
         // initialize both PICs
-        outb(0x20, 0x11);
-        outb(0xA0, 0x11);
+        outb(PIC1_CMD_IO_PORT, ICW1);
+        outb(PIC2_CMD_IO_PORT, ICW1);
 
         // set vector offset of pic1 to 0x20
-        outb(0x21, 0x20);
+        outb(PIC1_DATA_IO_PORT, PIC1_VECTOR_OFFSET);
         // set vector offset of pic2 to 0x28
-        outb(0xA1, 0x28);
+        outb(PIC2_DATA_IO_PORT, PIC2_VECTOR_OFFSET);
 
         // tell PIC1 about PIC2 at IRQ2 (0000 0100)
-        outb(0x21, 4);
+        outb(PIC1_DATA_IO_PORT, 4);
 
         // tell PIC2 its cascade identity (0000 0010)
-        outb(0xA1, 2);
+        outb(PIC2_DATA_IO_PORT, 2);
 
         // set both PICs to 8086 mode
-        outb(0x21, 0x1);
-        outb(0xA1, 0x1);
+        outb(PIC1_DATA_IO_PORT, ICW4);
+        outb(PIC2_DATA_IO_PORT, ICW4);
 
         // restore masks
-        outb(0x21, pic1_mask);
-        outb(0xA1, pic2_mask);
+        outb(PIC1_DATA_IO_PORT, pic1_mask);
+        outb(PIC2_DATA_IO_PORT, pic2_mask);
     }
 }
 
