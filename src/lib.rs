3f897aeabc0c4e1f1327afc10c1da850c9471929
myrrlyn <myrrlyn@outlook.com>
2016-10-31 08:27:24 -0400

    Forgot these:/

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 #![feature(const_fn)]
 
-// #![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_std)]
 
-#![no_std]
+#[cfg(test)]
+extern crate core;
 
 extern crate console;
 extern crate interrupts;
